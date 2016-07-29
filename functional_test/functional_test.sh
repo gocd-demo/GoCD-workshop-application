f30bab70343101ad5a7d90fdd666ce3afaa1ebed
Ken Mugrage <kmugrage@thoughtworks.com>
2016-07-29 10:53:05 -0700

    adding test report

--- functional_test/functional_test.sh
+++ functional_test/functional_test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "Running functional tests $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT"
+
+mkdir -p testoutput
+echo "This is the output from $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT" >> testoutput/output.txt
