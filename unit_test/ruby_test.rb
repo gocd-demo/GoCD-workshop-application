4edf52e0f38f09a3bf092c96b391e0858010ced3
Ken Mugrage <kmugrage@thoughtworks.com>
2016-08-15 17:59:04 -0700

    made test fail

--- unit_test/ruby_test.rb
+++ unit_test/ruby_test.rb
@@ -6,8 +6,8 @@ class TestMath < Minitest::Test
     assert_equal 2, 2
   end
 
-  # def test_math_should_fail
-  #   assert_equal 2, 3
-  # end
+  def test_math_should_fail
+    assert_equal 2, 3
+  end
 
 end
