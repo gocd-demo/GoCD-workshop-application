960b27570088c60bf788282cca382bd4101eb6e5
Ken Mugrage <kmugrage@thoughtworks.com>
2017-03-13 09:16:56 -0700

    modified deploy to use environment variables

--- deploy/deploy_application.sh
+++ deploy/deploy_application.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 set -e
 
-echo "Deploying to $1"
+# if [ -z $DEPLOY_ENVIRONMENT ]; then echo "No Deployment Environment"; exit; fi
+
+case "$DEPLOY_ENVIRONMENT" in
+	development)
+		echo "Deploying to development"
+		;;
+	functional_test)
+		echo "Deploying to functional_test"
+		;;
+	user_acceptance)
+		echo "Deploying to user_acceptance"
+		;;
+	staging)
+		echo "Deploying to staging"
+		;;
+	production)
+		echo "Deploying to production"
+		;;
+	*)
+		echo "No valid deployment target given"
+		exit 1
+		;;
+esac
+
+# echo "Deploying to $DEPLOY_ENVIRONMENT"
