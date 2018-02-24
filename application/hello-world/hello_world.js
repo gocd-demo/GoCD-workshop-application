388d154bfc9a72981c12d5ba80837901a06ac805
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-02-24 23:44:21 +0530

    Update the web app.

--- application/hello-world/hello_world.js
+++ application/hello-world/hello_world.js
@@ -16,11 +16,15 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (request, response) => {
   response.render('home', {
-    server_url: process.env.GO_SERVER_URL
+    pipeline_name: process.env.GO_PIPELINE_NAME,
+    pipeline_counter: process.env.GO_PIPELINE_COUNTER,
+    stage_name: process.env.GO_STAGE_NAME,
+    stage_counter: process.env.GO_STAGE_COUNTER,
+    job_name: process.env.GO_JOB_NAME
   })
 })
 
-var server = app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var server = app.listen(process.env.SERVER_PORT, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}!`))
 
 exports.closeServer = function(){
   server.close();
