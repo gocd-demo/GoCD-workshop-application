8f2b1a1e1b8b22e4c957a87a8f11a95086557d59
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-02-25 10:22:31 +0530

    Update deploy script.

--- deploy/deploy_application.sh
+++ deploy/deploy_application.sh
@@ -7,28 +7,28 @@ case "$DEPLOY_ENVIRONMENT" in
 	development)
 		echo "Deploying to development"
 		echo "Number of machines set to 1"
-	sh -c "cd application/hello-world/; npm install; SERVER_PORT=3004 nohup node hello_world.js &"
+		sh -c "cd application/hello-world/; npm install; SERVER_PORT=3004 nohup node hello_world.js >>/dev/null 2>&1 &"
 		;;
 
 	# Our testing environments use a 2 machine cluster
 	functional_test)
 		echo "Deploying to functional_test"
 		echo "Number of machines set to 2"
-		sh -c "npm install --prefix application/hello-world; SERVER_PORT=3003 nohup node application/hello-world/hello_world.js &>/dev/null"
+		sh -c "npm install --prefix application/hello-world; SERVER_PORT=3003 nohup node application/hello-world/hello_world.js >>/dev/null 2>&1 &"
 		;;
 
 	# Our testing environments use a 2 machine cluster
 	user_acceptance)
 		echo "Deploying to user_acceptance"
 		echo "Number of machines set to 2"
-		sh -c "npm install --prefix application/hello-world; SERVER_PORT=3002 nohup node application/hello-world/hello_world.js &>/dev/null"
+		sh -c "npm install --prefix application/hello-world; SERVER_PORT=3002 nohup node application/hello-world/hello_world.js >>/dev/null 2>&1 &"
 		;;
 
 	# Our staging environment is 20 machines in a production like cluster
 	staging)
 		echo "Deploying to staging"
 		echo "Number of machines set to 20"
-		sh -c "npm install --production --prefix application/hello-world; SERVER_PORT=3001 nohup node application/hello-world/hello_world.js &>/dev/null"
+		sh -c "npm install --production --prefix application/hello-world; SERVER_PORT=3001 nohup node application/hello-world/hello_world.js >>/dev/null 2>&1 &"
 		;;
 
 	# Our production environment is 200 machines
@@ -36,7 +36,7 @@ case "$DEPLOY_ENVIRONMENT" in
 		sh deploy/stop_server.sh
 		echo "Deploying to production"
 		echo "Number of machines set to 200"
-		sh -c "npm install --production --prefix application/hello-world; SERVER_PORT=3000 nohup node application/hello-world/hello_world.js &>/dev/null"
+		sh -c "npm install --production --prefix application/hello-world; SERVER_PORT=3000 nohup node application/hello-world/hello_world.js >>/dev/null 2>&1 &"
 		;;
 
 	# Fail the script if we don't get a valid environment
