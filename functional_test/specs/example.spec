98e545414bd51cf49683eb36b75fb1c9f120a436
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-03-07 12:10:58 +0530

    Add more gauge specs.

--- functional_test/specs/example.spec
+++ functional_test/specs/example.spec
@@ -3,3 +3,4 @@
 ## Get Started
 * Go to homepage at "http://localhost:3000"
 * Check title is "Hello World"
+* Check "Hello World!" text
--- functional_test/tests/step_implementation.js
+++ functional_test/tests/step_implementation.js
@@ -1,6 +1,6 @@
 /* globals gauge*/
 "use strict";
-const { openBrowser, closeBrowser, goto, click, text, title } = require('taiko');
+const { openBrowser, closeBrowser, goto, click, text, title, $, get } = require('taiko');
 const assert = require("assert");
 
 beforeSuite(async () => openBrowser());
@@ -12,4 +12,10 @@ step("Go to homepage at <query>", async query => goto(query));
 step("Check title is <word>", async (word) => {
   let t = await title()
   assert.equal(t, word)
+})
+
+
+step("Check <word> text", async (word) => {
+ text_to_assert_on = await text(word).exists();
+ assert.equal(text_to_assert_on, true)
 })
