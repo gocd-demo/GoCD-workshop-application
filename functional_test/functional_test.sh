047dfc75da345e28da847fbccc54f0d12193b6dd
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-03-09 14:45:22 +0530

    Add the gauge run command  to functional_test.sh

--- functional_test/functional_test.sh
+++ functional_test/functional_test.sh
@@ -3,5 +3,6 @@ set -e
 
 echo "Running functional tests $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT"
 
+npm install; gauge install; gauge run specs
 mkdir -p testoutput
 echo "This is the output from $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT" >> testoutput/output.txt
