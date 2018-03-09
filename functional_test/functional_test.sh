6dfba62ef3797f4d37bb73bd1743ad403a53f7ca
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-03-09 15:46:22 +0530

    Set working dir.

--- functional_test/functional_test.sh
+++ functional_test/functional_test.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Running functional tests $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT"
 
-npm install; gauge install; gauge run specs
+npm install --prefix functional_test; gauge install -d functional_test; gauge run specs -d functional_test
+
 mkdir -p testoutput
 echo "This is the output from $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT" >> testoutput/output.txt
