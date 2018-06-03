a77c5a9f1e56314a1401929676d71e2bea1f1b49
Ken Mugrage <kmugrage@users.noreply.github.com>
2018-06-03 10:06:16 -0700

    Update functional_test.sh

--- functional_test/functional_test.sh
+++ functional_test/functional_test.sh
@@ -3,4 +3,6 @@ set -e
 
 echo "Running functional tests $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT"
 
+mkdir -p testoutput
+
 echo "This is the output from $GO_JOB_RUN_INDEX of $GO_JOB_RUN_COUNT" >> testoutput/output.txt
